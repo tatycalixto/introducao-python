adc91a785fda0829a890cce5f8c44d7c931c760b
Taty Calixto <54193277+tatycalixto@users.noreply.github.com>
2023-06-08 20:57:14 -0300

    aula06

---
+++ Aula 06/Lista6-Q6.py
@@ -0,0 +1,4 @@
+def calcular(n1,n2,n3):
+    print("Soma = ",n1+n2+n3)
+    
+calcular(100,20,30)
---
+++ Aula 06/Lista6-Q7.py
@@ -0,0 +1,8 @@
+def verificarNumero(numero):
+    if numero > 0:
+        print("P")
+    elif numero < 0 or numero == 0:
+        print("N")
+
+numero2 = -6
+verificarNumero(numero2)
